f9e6cf6e440b2038456b9b584a842951590939a7
quenio <queniodossantos@gmail.com>
2020-03-21 10:30:33 -0300

    Implemented MiniSuave.

--- Program.fs
+++ Program.fs
@@ -1,8 +1,22 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
+open Suave.Console
+open Suave.Http
+open Suave.Successful
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let request =
+        { Route = ""
+          Type = Suave.Http.GET }
+
+    let response =
+        { Content = ""
+          StatusCode = 200 }
+
+    let context =
+        { Request = request
+          Response = response }
+
+    execute context (OK "Hello Suave!")
+    0
--- Suave.fs
+++ Suave.fs
@@ -1,2 +1,48 @@
 namespace Suave
 
+module Http =
+    type RequestType =
+        | GET
+        | POST
+
+    type Request =
+        { Route: string
+          Type: RequestType }
+
+    type Response =
+        { Content: string
+          StatusCode: int }
+
+    type Context =
+        { Request: Request
+          Response: Response }
+
+    type WebPart = Context -> Async<Context option>
+
+module Successful =
+    open Http
+
+    let OK content context =
+        { context with
+              Response =
+                  { Content = content
+                    StatusCode = 200 } }
+        |> Some
+        |> async.Return
+
+module Console =
+    open Http
+
+    let execute inputContext webPart =
+        async {
+            let! outputContext = webPart inputContext
+            printfn "---"
+            match outputContext with
+            | Some context ->
+                printfn "Code: %d" context.Response.StatusCode
+                printfn "Output: %s" context.Response.Content
+            | None ->
+                printfn "No Output"
+            printfn "---"
+        }
+        |> Async.RunSynchronously
